fe2109c69c745862be90a466e803813fcb76860c
ahmadnasir123 <nasirahmad240502@gmail.com>
2024-06-06 21:45:31 +0700

    update backsite role controller

--- app/Http/Controllers/backsite/RoleController.php
+++ app/Http/Controllers/backsite/RoleController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\backsite;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\ManagementAccess\Role;
+use App\Models\ManagementAccess\Permission;
+use App\Http\Requests\Role\StoreRoleRequest;
+use App\Http\Requests\Role\UpdateRoleRequest;
 
 class RoleController extends Controller
 {
@@ -41,40 +44,62 @@ class RoleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRoleRequest $request)
     {
-        return abort(404);
+        // get all request from fronsite
+        $data = $request->all();
+
+        // store to database
+        $role = Role::create($data);
+
+        alert()->success('Success Message', 'Successfully added new role');
+        return redirect()->route('backsite.role.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Role $role)
     {
-        return abort(404);
+        // need more notes here
+        $role->load('permission');
+
+        return view('pages.backsite.management-access.role.show', compact('role'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Role $role)
     {
-        return abort(404);
+        // need more notes here
+        $permission = Permission::all();
+        $role->load('permission');
+
+        return view('pages.backsite.management-access.role.edit', compact('permission', 'role'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRoleRequest $request, Role $role)
     {
-        return abort(404);
+        // need more notes here
+        $role->update($request->all());
+        $role->permission()->sync($request->input('permission', []));
+
+        alert()->success('Success Message', 'Successfully updated role');
+        return redirect()->route('backsite.role.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        return abort(404);
+        $role->forceDelete();
+
+        alert()->success('Success Message', 'Successfully deleted role');
+        return back();
     }
 }
