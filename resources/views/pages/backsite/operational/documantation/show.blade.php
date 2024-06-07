f69bed2f61cf05ef0598d4d5b9a2a0abc362f354
ahmadnasir123 <nasirahmad240502@gmail.com>
2024-06-07 23:58:10 +0700

    add backsite pages documantation show

---
+++ resources/views/pages/backsite/operational/documantation/show.blade.php
@@ -0,0 +1,25 @@
+<table class="table table-bordered">
+    <tr>
+        <th>Nama Kegiatan</th>
+        <td>{{ isset($documantation->name) ? $documantation->name : 'N/A' }}</td>
+    </tr>
+    <tr>
+        <th>Deskripsi Kegiatan</th>
+        <td>{{ isset($documantation->description) ? $documantation->description : 'N/A' }}</td>
+    </tr>
+    <tr>
+        <th>Photo</th>
+        <td>
+            <img src="
+                @if ($documantation->photo != "")
+                    @if(File::exists('storage/'.$documantation->photo))
+                        {{ url(Storage::url($documantation->photo)) }}
+                    @else
+                       {{ 'N/A' }}
+                    @endif
+                @else
+                    {{ 'N/A' }}
+                @endif " alt="documantation photo" class="users-avatar-shadow" height="100" width="100">
+        </td>
+    </tr>
+</table>
